63795595af8d1c67eb2559de41c1339af7e98602
HoangTien <hvtien911@gmail.com>
2021-08-25 08:29:33 +0700

    CRUD Firebase - OK

---
+++ QuanLyThuVien/Areas/Admin/Controllers/ql_SachController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp.Response;
+using QuanLyThuVien.Models;
+using Newtonsoft.Json.Serialization;
+
+namespace QuanLyThuVien.Areas.Admin.Controllers
+{
+    public class ql_SachController : Controller
+    {
+        // GET: Admin/ql_Sach
+        IFirebaseClient client;
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            BasePath = "https://libmanagerdatabase-default-rtdb.asia-southeast1.firebasedatabase.app/",
+            AuthSecret = "Sxg7VD8YEx8nLTf7SJSSFK8c4ZWfKzvBokW1uw25"
+        };
+        //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv SHOW vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+        public ActionResult ListBooks()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Books");            
+            Dictionary<string, Books> data = JsonConvert.DeserializeObject<Dictionary<string, Books>>(response.Body.ToString());
+            var list = new List<Books>();
+            foreach (var item in data)
+            {
+                Books book = new Books();
+                book._id = item.Value._id;
+                book.title = item.Value.title;
+                book.authors = item.Value.authors;
+                book.categories = item.Value.categories;
+                book.shortDescription = item.Value.shortDescription;
+                book.longDescription = item.Value.longDescription;
+                book.count = item.Value.count;
+                book.publishedDate = item.Value.publishedDate;
+                book.status = item.Value.status;
+                book.thumbnailUrl = item.Value.thumbnailUrl;
+                list.Add(book);
+            }
+            return View(list);
+        }
+        //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv DETAIL vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+        [HttpGet]
+        public ActionResult Detail(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Books/" + id);
+            Books data = JsonConvert.DeserializeObject<Books>(response.Body);
+            return View(data);
+        }
+        //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv CREATE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Books book)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            PushResponse response = client.Push("Books/", book);
+            book._id = response.Result.name;
+            client.Set("Books/" + book._id, book);
+            return View();
+        }
+
+        //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv UPDATE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Books/" + id);
+            Books data = JsonConvert.DeserializeObject<Books>(response.Body);
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Books book)
+        {
+            client = new FireSharp.FirebaseClient(config);          
+            client.Set("Books/" + book._id, book);           
+            return RedirectToAction("ListBooks");
+        }
+
+
+        //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv DELETE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+        public ActionResult Delete(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            client.Delete("Books/" + id);
+            return RedirectToAction("ListBooks");
+        }
+    }
+}
---
+++ QuanLyThuVien/Models/Books.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyThuVien.Models
+{
+    public class Books
+    {
+        [Display(Name = "ID")]
+        public string _id { get; set; }
+        [Display(Name = "Tên sách")]
+        [Required(ErrorMessage = "Vui lòng nhập tên sách")]
+        public string title { get; set; }
+        [Display(Name = "Tác giả")]
+        [Required(ErrorMessage = "Vui lòng nhập tác giả")]
+        public string authors { get; set; }
+        [Display(Name = "Thể loại")]
+        [Required(ErrorMessage = "Vui lòng nhập thể loại")]
+        public string categories { get; set; }
+        [Display(Name = "Giới thiệu")]
+        [Required(ErrorMessage = "Vui lòng nhập giới thiệu")]
+        public string shortDescription { get; set; }
+        [Display(Name = "Giới thiệu chi tiết")]
+        [Required(ErrorMessage = "Vui lòng nhập giới thiệu chi tiết")]
+        public string longDescription { get; set; }
+        [Display(Name = "Số lượng")]
+        [Required(ErrorMessage = "Vui lòng nhập số lượng")]
+        public int count { get; set; }
+        [Display(Name = "Ngày phát hành")]
+        [Required(ErrorMessage = "Vui lòng nhập ngày phát hành")]
+        public DateTime publishedDate { get; set; }
+        [Display(Name = "Trạng thái")]
+        [Required(ErrorMessage = "Vui lòng nhập trạng thái")]
+        public string status { get; set; }
+        [Display(Name = "Ảnh bìa")]
+        [Required(ErrorMessage = "Vui lòng nhập link ảnh bìa")]
+        public string thumbnailUrl { get; set; }      
+    }
+}
