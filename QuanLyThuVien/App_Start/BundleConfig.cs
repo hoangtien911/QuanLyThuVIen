840c1cee9505d9aa184a7d45ee323359eedef8f2
HoangTien <hvtien911@gmail.com>
2021-09-08 22:16:05 +0700

    - Sửa lại một số text Admin - Thực hiện tạo giao diện người dùng - Tiến độ giao diện người dùng: 5%

--- QuanLyThuVien/App_Start/BundleConfig.cs
+++ QuanLyThuVien/App_Start/BundleConfig.cs
@@ -9,26 +9,15 @@ namespace QuanLyThuVien
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
+                        "~/Scripts/jquery-{version}.js"));         
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+                        "~/Scripts/LibraryJs/modernizr-*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Scripts/bootstrap.js"));     
             bundles.Add(new ScriptBundle("~/bundles/chosen").Include(
-    "~/Scripts/chosen.jquery.js",
-    "~/Scripts/prism.js",
-    "~/Scripts/init.js"));
+                      "~/Scripts/chosen/chosen.jquery.js"));          
         }
     }
 }
---
+++ QuanLyThuVien/Controllers/TrangChuController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp.Response;
+using QuanLyThuVien.Models;
+
+namespace QuanLyThuVien.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: TrangChu
+        IFirebaseClient client;
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            BasePath = "https://libmanagerdatabase-default-rtdb.asia-southeast1.firebasedatabase.app/",
+            AuthSecret = "Sxg7VD8YEx8nLTf7SJSSFK8c4ZWfKzvBokW1uw25"
+        };
+        public ActionResult HomePage()
+        {
+            ViewBag.listbooks = ListBooks();
+            return View();
+        }
+
+        
+        //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv DANH SÁCH SÁCH vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+        public List<Books> ListBooks()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Books");
+            Dictionary<string, Books> data = JsonConvert.DeserializeObject<Dictionary<string, Books>>(response.Body.ToString());
+            var list = new List<Books>();
+            foreach (var item in data)
+            {
+                Books book = new Books();
+                book._id = item.Value._id;
+                book.title = item.Value.title;
+                book.authors = item.Value.authors;
+                book.categories = item.Value.categories;
+                book.shortDescription = item.Value.shortDescription;
+                book.longDescription = item.Value.longDescription;
+                book.count = item.Value.count;
+                book.publishedDate = item.Value.publishedDate;
+                book.status = item.Value.status;
+                book.thumbnailUrl = item.Value.thumbnailUrl;
+                list.Add(book);
+            }           
+            return list;
+        }
+    }
+}
